49fc0b8b886a8e0464676db8a4df74e679a71cca
akansha21-oss <akanshajoshi845@gmail.com>
2025-04-02 16:55:09 +0530

    blog updates

--- Tours/views.py
+++ Tours/views.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 
 # Define the tours view function tours
 def serviceTours(request):
-    blogs = Blog.objects.filter(active=True, blog_type='tours').order_by("-published_date")
+    blogs = Blog.objects.filter(active=True, blog_type='tour').order_by("-published_date")
     
     # Validate and fix slugs if needed
     for blog in blogs:
